bc28370c88cc561b14631d05c1deafa570683af9
Roberto Carreño <r.carrenoz@uniandes.edu.co>
2021-05-16 23:35:44 -0500

    Se incluye prueba backstopjs

---
+++ analizador-backstopjs/backstop_data/html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Inicio_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210516-233303\\backstop_default_Inicio_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Inicio_0_document_0_default.png",
+        "label": "Inicio",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "file:///D:/OneDrive/Documents/Maestr%C3%ADa/3.%20Pruebas%20automatizadas%20de%20Software/Semana%206/backstopjs-page/v3425/report.html",
+        "referenceUrl": "https://robertopabloc.github.io/backstopjs-page/v330/report.html",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
